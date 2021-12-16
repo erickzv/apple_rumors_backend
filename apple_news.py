c0325c8b24f582053bbcc3c6b02cb3a433dfbb01
Erick ZV <zv.erick@protonmail.com>
2021-12-15 22:50:59 -0600

    Initial commit

---
+++ apple_news.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+mac_rumors = {
+    "url": "https://macrumors.com",
+    "find_tag": "h2"
+}
+
+apple_insiders = {
+    "url": "https://appleinsider.com",
+    "find_tag": "h2"
+}
+
+nine_to_five_mac = {
+    "url": "https://9to5mac.com",
+    "find_tag": "h1"
+}
+
+
+def get_news(url: str, find_tag: str):
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    h2_tags = soup.find_all(find_tag)
+
+    rumors = []
+    for tag in h2_tags:
+        a_tag = tag.find("a")
+        if a_tag is not None:
+            rumors.append(
+                {
+                    "title": a_tag.text,
+                    "href": a_tag.get("href")
+                }
+            )
+        if len(rumors) == 10:
+            break
+    return rumors
---
+++ main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+import apple_news
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["OPTIONS", "POST", "GET", "DELETE"],
+    allow_headers=["*"],
+)
+
+news_urls = [
+    apple_news.mac_rumors,
+    apple_news.apple_insiders,
+    apple_news.nine_to_five_mac
+]
+
+
+@app.get("/all_news")
+def all_news():
+    news = []
+    domains = []
+    for data in news_urls:
+        news.append(apple_news.get_news(**data))
+        domains.append(data["url"][8:-4])
+    news.append(domains)
+    return news
+
+
+@app.get("/macrumors")
+def mac_rumors():
+    news = apple_news.get_news(**apple_news.mac_rumors)
+    return news
+
+
+@app.get("/apple_insiders")
+def apple_insiders():
+    news = apple_news.get_news(**apple_news.apple_insiders)
+    return news
+
+
+@app.get("/9to5mac")
+def nine_to_five_mac():
+    news = apple_news.get_news(**apple_news.nine_to_five_mac)
+    return news
